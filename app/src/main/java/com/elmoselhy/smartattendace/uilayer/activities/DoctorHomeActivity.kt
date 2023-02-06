3d3902648b50b229b5f6058b4ea869368f7f63ed
ahmedelmoselhy98 <ahmedelmoselhy98@gmail.com>
2023-02-06 08:58:03 +0200

    Commit // nfc

--- app/src/main/java/com/elmoselhy/smartattendace/uilayer/activities/DoctorHomeActivity.kt
+++ app/src/main/java/com/elmoselhy/smartattendace/uilayer/activities/DoctorHomeActivity.kt
@@ -1,19 +1,17 @@
 package com.elmoselhy.smartattendace.uilayer.activities
 
-import android.R
-import android.R.attr.bitmap
 import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.Color
+import android.nfc.NfcAdapter
 import android.view.View
-import androidmads.library.qrgenearator.QRGContents
-import androidmads.library.qrgenearator.QRGEncoder
+import android.widget.Toast
 import androidx.core.view.GravityCompat
 import com.elmoselhy.smartattendace.databinding.ActivityDoctorHomeBinding
 import com.elmoselhy.smartattendace.datalayer.session.Preference
+import com.elmoselhy.smartattendace.utilitiess.utils.NfcUtilities
 import net.glxn.qrgen.android.QRCode
 
-class DoctorHomeActivity : BaseActivity() {
+class DoctorHomeActivity : BaseActivity() , NfcUtilities.NfcActivity {
     lateinit var binding: ActivityDoctorHomeBinding
 
     override fun setUpLayoutView(): View {
@@ -22,6 +20,17 @@ class DoctorHomeActivity : BaseActivity() {
     }
 
     override fun init() {
+        var nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        val isNfcSupported: Boolean = nfcAdapter != null
+
+        if (isNfcSupported) {
+            Toast.makeText(this, "Nfc is supported on this device", Toast.LENGTH_SHORT).show()
+        } else Toast.makeText(this, "Nfc is not supported on this device", Toast.LENGTH_SHORT)
+            .show()
+        var nfcUtilities = NfcUtilities(this)
+        nfcAdapter?.setOnNdefPushCompleteCallback(nfcUtilities, this)
+        nfcAdapter?.setNdefPushMessageCallback(nfcUtilities, this)
+
         setUpPageActions()
         setUSerData()
         qrCodeGenerator()
@@ -77,6 +86,17 @@ class DoctorHomeActivity : BaseActivity() {
                     finishAffinity()
                 }
             })
+        }
+    }
+
+
+    override fun getOutcomingMessage(): String {
+        return preference.getUserSession()?.id!!
+    }
+
+    override fun signalResult() {
+        runOnUiThread {
+            Toast.makeText(this, "Nfc Send Success", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/elmoselhy/smartattendace/uilayer/activities/StudentHomeActivity.kt
+++ app/src/main/java/com/elmoselhy/smartattendace/uilayer/activities/StudentHomeActivity.kt
@@ -1,16 +1,25 @@
 package com.elmoselhy.smartattendace.uilayer.activities
 
+import android.app.PendingIntent
+import android.content.ClipDescription
 import android.content.Intent
+import android.content.IntentFilter
+import android.nfc.NdefMessage
+import android.nfc.NfcAdapter
+import android.nfc.NfcEvent
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidmads.library.qrgenearator.QRGContents
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
+import com.elmoselhy.smartattendace.R
 import com.elmoselhy.smartattendace.databinding.ActivityStudentHomeBinding
 import com.elmoselhy.smartattendace.datalayer.models.AttendanceModel
 import com.elmoselhy.smartattendace.datalayer.models.DoctorModel
 import com.elmoselhy.smartattendace.datalayer.models.StudentModel
 import com.elmoselhy.smartattendace.datalayer.session.Preference
+import com.elmoselhy.smartattendace.utilitiess.utils.NfcUtilities
 import com.elmoselhy.smartattendace.utilitiess.utils.Utils
 import io.github.g00fy2.quickie.QRResult
 import io.github.g00fy2.quickie.ScanQRCode
@@ -25,6 +34,7 @@ class StudentHomeActivity : BaseActivity() {
 
     override fun init() {
         setUpPageActions()
+
     }
 
     private fun setUpPageActions() {
@@ -68,30 +78,7 @@ class StudentHomeActivity : BaseActivity() {
         // handle QRResult
         when (result) {
             is QRResult.QRSuccess -> {
-                firebaseController.getUser(result.content.rawValue, onResult = {
-                    it?.let {
-                        var attendanceModel = AttendanceModel()
-                        attendanceModel.date = Utils.formatDate(Date())
-                        var doctorModel = DoctorModel()
-                        doctorModel.id = it!!.id!!
-                        doctorModel.fullName = it.fullName!!
-                        doctorModel.subjectName = it.subjectName!!
-                        attendanceModel.doctor = doctorModel
-                        var studentModel = StudentModel()
-                        studentModel.id = preference.getUserSession()!!.id!!
-                        studentModel.fullName = preference.getUserSession()!!.fullName!!
-                        studentModel.studentCode = preference.getUserSession()!!.studentCode!!
-                        attendanceModel.student = studentModel
-                        firebaseController.setStudentAttendance(
-                            result.content.rawValue,
-                            attendanceModel,
-                            onResult = { success ->
-                                if (success) {
-                                    Toast.makeText(this, "تعيين حضور", Toast.LENGTH_SHORT).show()
-                                }
-                            })
-                    }
-                })
+                setMeAttendance(result.content.rawValue)
             }
             QRResult.QRUserCanceled -> Toast.makeText(this, "تم الإلفاء", Toast.LENGTH_SHORT).show()
             QRResult.QRMissingPermission -> Toast.makeText(
@@ -102,4 +89,100 @@ class StudentHomeActivity : BaseActivity() {
             is QRResult.QRError -> "${result.exception.javaClass.simpleName}: ${result.exception.localizedMessage}"
         }
     }
+
+    private fun setMeAttendance(doctorId: String) {
+        firebaseController.getUser(doctorId, onResult = {
+            it?.let {
+                var attendanceModel = AttendanceModel()
+                attendanceModel.date = Utils.formatDate(Date())
+                var doctorModel = DoctorModel()
+                doctorModel.id = it!!.id!!
+                doctorModel.fullName = it.fullName!!
+                doctorModel.subjectName = it.subjectName!!
+                attendanceModel.doctor = doctorModel
+                var studentModel = StudentModel()
+                studentModel.id = preference.getUserSession()!!.id!!
+                studentModel.fullName = preference.getUserSession()!!.fullName!!
+                studentModel.studentCode = preference.getUserSession()!!.studentCode!!
+                attendanceModel.student = studentModel
+                firebaseController.setStudentAttendance(
+                    doctorId,
+                    attendanceModel,
+                    onResult = { success ->
+                        if (success) {
+                            Toast.makeText(this, "تعيين حضور", Toast.LENGTH_SHORT).show()
+                        }
+                    })
+            }
+        })
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        // foreground dispatch should be enabled here, as onResume is the guaranteed place where app
+        // is in the foreground
+//        enableForegroundDispatch(this, this.nfcAdapter)
+        receiveMessageFromDevice(intent)
+    }
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        // also reading NFC message from here in case this activity is already started in order
+        // not to start another instance of this activity
+        receiveMessageFromDevice(intent)
+    }
+
+    private fun receiveMessageFromDevice(intent: Intent) {
+        val action = intent.action
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED == action) {
+            val parcelables = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+            with(parcelables) {
+                this?.let {
+                    val inNdefMessage = it[0] as NdefMessage
+                    val inNdefRecords = inNdefMessage.records
+                    val ndefRecord_0 = inNdefRecords[0]
+                    val inMessage = String(ndefRecord_0.payload)
+                    Toast.makeText(
+                        this@StudentHomeActivity,
+                        "DoctorId: $inMessage",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+    }
+//    private fun enableForegroundDispatch(activity: AppCompatActivity, adapter: NfcAdapter?) {
+//
+//        // here we are setting up receiving activity for a foreground dispatch
+//        // thus if activity is already started it will take precedence over any other activity or app
+//        // with the same intent filters
+//
+//        val intent = Intent(activity.applicationContext, activity.javaClass)
+//        intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+//
+//        val pendingIntent = PendingIntent.getActivity(activity.applicationContext, 0, intent, 0)
+//
+//        val filters = arrayOfNulls<IntentFilter>(1)
+//        val techList = arrayOf<Array<String>>()
+//
+//        filters[0] = IntentFilter()
+//        with(filters[0]) {
+//            this?.addAction(NfcAdapter.ACTION_NDEF_DISCOVERED)
+//            this?.addCategory(Intent.CATEGORY_DEFAULT)
+//            try {
+//                this?.addDataType(ClipDescription.MIMETYPE_TEXT_PLAIN)
+//            } catch (ex: IntentFilter.MalformedMimeTypeException) {
+//                throw RuntimeException("Check your MIME type")
+//            }
+//        }
+//
+//        adapter?.enableForegroundDispatch(activity, pendingIntent, filters, techList)
+//    }
+//    private fun disableForegroundDispatch(activity: AppCompatActivity, adapter: NfcAdapter?) {
+//        adapter?.disableForegroundDispatch(activity)
+//    }
+//    override fun onPause() {
+//        super.onPause()
+//        disableForegroundDispatch(this, this.nfcAdapter)
+//    }
 }
